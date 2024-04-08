154254058a141feb22d098ec8f0be24695bcf6b3
Dawid Furman <dfurmans@gmail.com>
2024-04-08 19:54:53 +0200

    Refector Main method - extract common logic for for-comprehension loop

--- src/main/scala/co/icoworking/simplehttpservice/SimplehttpserviceServer.scala
+++ src/main/scala/co/icoworking/simplehttpservice/SimplehttpserviceServer.scala
@@ -1,52 +1,50 @@
 package co.icoworking.simplehttpservice
 
 import cats.effect.kernel.Resource
-import cats.effect.{Async}
+import cats.effect.Async
 import co.icoworking.simplehttpservice.repository.TareaRepository
 import co.icoworking.simplehttpservice.service.{TareaService, UserService}
 import com.comcast.ip4s.*
 import doobie.Transactor
 import fs2.io.net.Network
-import org.http4s.ember.client.EmberClientBuilder
+//import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits.*
 import org.http4s.server.middleware.Logger
 import cats.implicits.toSemigroupKOps
+import org.http4s.HttpApp
 
 object SimplehttpserviceServer:
   def run[F[_]: Async: Network]: F[Nothing] = {
-    val k: Resource[F, Unit] = for {
-      client <- EmberClientBuilder.default[F].build // todo Change me
-      //helloWorldAlg: HelloWorld[F] = HelloWorld.impl[F]
 
-      xa = Transactor.fromDriverManager[F](
+    val init: HttpApp[F] = {
+      val xa = Transactor.fromDriverManager[F](
         driver = "org.postgresql.Driver", // JDBC driver classname
-        url = "jdbc:postgresql:world",    // Connect URL
-        user = "postgres",                // Database user name
-        password = "password",            // Database password
-        logHandler = None                 // Don't setup logging for now. See Logging page for how to log events in detail
+        url = "jdbc:postgresql:world", // Connect URL
+        user = "postgres", // Database user name
+        password = "password", // Database password
+        logHandler = None // Don't setup logging for now. See Logging page for how to log events in detail
       )
-      repositorioUser = new co.icoworking.simplehttpservice.repository.UserRepository(xa)
       // user http service
-      userService = UserService.impl[F](ur = repositorioUser)
-      repositorioTarea = new TareaRepository(xa)
-      tareaService = TareaService.impl[F](tr = repositorioTarea)
-      //resolver dependencias
+      val userService = UserService.impl[F](ur = new co.icoworking.simplehttpservice.repository.UserRepository(xa))
+      val tareaService = TareaService.impl[F](tr = new TareaRepository(xa))
 
-      httpApp = (
-        //SimplehttpserviceRoutes.helloWorldRoutes[F](helloWorldAlg) <+>
-          SimplehttpserviceRoutes.userServiceRoutes[F](userService)   <+>
+      val httpApp = (
+        SimplehttpserviceRoutes.userServiceRoutes[F](userService) <+>
           SimplehttpserviceRoutes.tareaServiceRoutes[F](tareaService)
-      ).orNotFound
+        ).orNotFound
 
-      finalHttpApp = Logger.httpApp(true, true)(httpApp)
-
-      _ <- 
-        EmberServerBuilder.default[F]
-          .withHost(ipv4"0.0.0.0")
-          .withPort(port"8080")
-          .withHttpApp(finalHttpApp)
+      Logger.httpApp(true, true)(httpApp)
+    }
+    val IpHttp: Ipv4Address = ipv4"0.0.0.0"
+    val PortHttp: Port = port"8080"
+    val application: Resource[F, Unit] = for {
+      _ <- EmberServerBuilder.default[F]
+          .withHost(IpHttp)
+          .withPort(PortHttp)
+          .withHttpApp(init)
           .build
-    } yield ()
-    k.useForever
+    } yield () // definicion de application // type Safe - > Nivel de los tipos
+
+    application.useForever // arrancamos el motor - app
   }
--- src/main/scala/co/icoworking/simplehttpservice/repository/Repository.scala
+++ src/main/scala/co/icoworking/simplehttpservice/repository/Repository.scala
@@ -8,6 +8,9 @@ import cats.effect.Async
 import doobie._
 import doobie.implicits._
 
+object Tables{
+  val UserTableName = "Usuario"
+}
 // Interface CRUD para Usuario
 trait UserRepositoryI[F[_] : Applicative]:
   def saveUser(usuarioParaGuardar: Usuario): F[Usuario]
@@ -15,25 +18,20 @@ trait UserRepositoryI[F[_] : Applicative]:
   def update(id: Int, updateData: Usuario): F[Usuario]
   def deleteUser(id: Int): F[Unit]
 
-
 trait TareaRepositoryI[F[_]: Applicative]:
   def saveTarea(guardarTarea: Tarea): F[Tarea]
   def encontrarPorId(id: Int): F[Tarea]
   
 class TareaRepository[F[_]: Applicative: Async](transactor: Transactor[F]) extends TareaRepositoryI[F]:
   def saveTarea(guardarTarea: Tarea): F[Tarea] =
-    val insertNewTareaSql = sql"INSERT INTO ...".query[Tarea].unique
+    val insertNewTareaSql = sql"INSERT INTO Tarea ()".query[Tarea].unique
     insertNewTareaSql.transact(transactor)
   def encontrarPorId(id: Int): F[Tarea] = ???
 
 
 class UserRepository[F[_] : Applicative: Async](transactor: Transactor[F]) extends UserRepositoryI[F]:
   override def saveUser(usuarioParaGuardar: Usuario): F[Usuario] =
-
-    val insertNewUserSQL = sql"INSERT INTO ...".query[Usuario].unique
-    //insertNewUserSQL.transact(transactor) // executa SQL en IO!!!!
-    //val program1: doobie.ConnectionIO[Int] = 42.pure[ConnectionIO]
-    println(s"$transactor $usuarioParaGuardar $insertNewUserSQL")
+    val insertNewUserSQL = sql"INSERT INTO ${Tables.UserTableName} ".query[Usuario].unique
     insertNewUserSQL.transact(transactor)
 
   override def findById(id: Int): F[Usuario] = ???

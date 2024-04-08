34bf4a86df1be1d03f093d72cf1f56a12a68ac4f
Dawid Furman <dfurmans@gmail.com>
2024-04-08 19:31:43 +0200

    Clean unreleated comments

--- src/main/scala/co/icoworking/simplehttpservice/service/UserService.scala
+++ src/main/scala/co/icoworking/simplehttpservice/service/UserService.scala
@@ -5,15 +5,11 @@ import co.icoworking.simplehttpservice.model.*
 import co.icoworking.simplehttpservice.repository.UserRepository
 // Iterface de Usuarion HTTP API
 trait UserService[F[_]: Applicative]:
-  def findUser(id: Int): F[Usuario] // HTTP GET
-  def saveUser(u: Usuario): F[Usuario] // HTTP POST
+  def findUser(id: Int): F[Usuario]
+  def saveUser(u: Usuario): F[Usuario]
 
 class UserServiceImpl[F[_] : Applicative](userRepository: UserRepository[F]) extends UserService[F]:
-  override def findUser(id: Int): F[Usuario] =
-    val usarioEncontrado = userRepository.findById(id)
-    println(s"$usarioEncontrado")
-    usarioEncontrado // fixed!!!! violaaa!
-    //user.pure[F] // fix me
+  override def findUser(id: Int): F[Usuario] = userRepository.findById(id)
   override def saveUser(u: Usuario): F[Usuario] = ??? // implemnt me
 
 object UserService:

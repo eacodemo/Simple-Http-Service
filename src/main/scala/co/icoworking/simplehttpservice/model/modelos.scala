4241e21f860d73369f1ef96d34018b1db22c88ac
eacodemo <eacodemo@gmail.com>
2024-04-09 22:33:44 -0500

    Se implemento modelo de proyecto

--- src/main/scala/co/icoworking/simplehttpservice/model/modelos.scala
+++ src/main/scala/co/icoworking/simplehttpservice/model/modelos.scala
@@ -4,30 +4,51 @@ import io.circe.{Encoder, Json}
 import org.http4s.EntityEncoder
 import org.http4s.circe.*
 
-final case class Usuario(id: Int,
-                   nombre: String,
-                   email: String,
-                   contraseña: String,
-                   tipo: String)
+import java.sql.Date
+
+final case class Usuario(
+                          id: Int,
+                          nombre: String,
+                          email: String,
+                          contraseña: String,
+                          tipo: String
+                        )
 object Usuario: // objecto companiero de ADT Usuario
   given Encoder[Usuario] = new Encoder[Usuario]: // Serializacion por Circe
     final def apply(usuario: Usuario): Json = Json.obj(
-      ("message", Json.fromString(usuario.tipo)),
+      ("tipo", Json.fromString(usuario.tipo)),
     )
   given [F[_]]: EntityEncoder[F, Usuario] = jsonEncoderOf[F, Usuario] // instancia implicit with given Scala 3 way
 
-case class Tarea(ID: Int,
-                       descripcion: String,
-                       estado: String,
-                       IDHistoriaUsuario: Int)
+case class Tarea(
+                  id: Int,
+                  descripcion: String,
+                  estado: String,
+                  idHistoriaUsuario: Int
+                )
 object Tarea:
   given Encoder[Tarea] = new Encoder[Tarea]:
     override def apply(tarea: Tarea): Json = Json.obj(
-      ("message", Json.fromString(tarea.estado)),
+      ("Estado", Json.fromString(tarea.estado)),
     )
 
   given [F[_]]: EntityEncoder[F, Tarea] = jsonEncoderOf[F, Tarea]
 
-//case class Proyecto(ID: Int, nombre: String, descripcion: Option[String], fechaInicio: Option[Date])
+case class Proyecto(
+                     id: Int,
+                     nombre: String,
+                     descripcion: Option[String],
+                     contraseña: String,
+                     fechaInicio: Option[Date]
+                   )
+object Proyecto:
+  given Encoder[Proyecto] = new Encoder[Proyecto]:
+    override def apply(proyecto: Proyecto): Json = Json.obj(
+      ("id", Json.fromInt(proyecto.id)),
+      ("nombre", Json.fromString(proyecto.nombre)),
+    )
+
+
+
 //case class AsociacionProyectoUsuario(ID: Int, IDProyecto: Int, IDUsuario: Int, Rol: String)
 //case class HistoriaUsuario(ID: Int, detalles: String, criteriosAceptacion: String, estado: String, IDProyecto: Int, IDTareaAsociada: Option[Int])
